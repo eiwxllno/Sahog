5edf269d48ade5ad41c821ca3c7b864b4a0c17b5
eiwxllno <erwinllano95@gmail.com>
2024-11-08 13:45:39 +0800

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '64bf2e61-5647-465e-8626-d3d81bf81597';
+export const MODAL_CLOSE_SEC = 2.5;
