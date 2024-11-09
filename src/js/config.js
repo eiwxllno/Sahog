b2838906b2b8222fcb72395bd966e62cf8b8a24f
eiwxllno <erwinllano95@gmail.com>
2024-11-09 13:20:41 +0800

    Change API key

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '64bf2e61-5647-465e-8626-d3d81bf81597';
+export const KEY = '7dd93cfc-48ff-4fbf-ab3f-31f0be71d5a9';
 export const MODAL_CLOSE_SEC = 2.5;
