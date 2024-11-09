35b8e1bf3b2611e1df33323ca3049fe18ebecce4
eiwxllno <erwinllano95@gmail.com>
2024-11-09 13:41:28 +0800

    Fix spacing

--- src/js/views/view.js
+++ src/js/views/view.js
@@ -12,6 +12,7 @@ export default class View {
    * @uthor Erwin Llano
    * @todo Finish implementation
    */
+
   render(data, render = true) {
     if (!data || (Array.isArray(data) && data.length === 0))
       return this.renderError();
